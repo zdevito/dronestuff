f8b89874e139109c2fa506729e158637ba239eb9
Zachary DeVito <zdevito@stanford.edu>
2013-04-05 16:16:26 -0700

    parsing navstream demo state.

--- Pidgen/Pidgen/AppDelegate.m
+++ Pidgen/Pidgen/AppDelegate.m
@@ -13,7 +13,129 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include "navdata_common.h"
+//#include "navdata_common.h"
+
+
+#define NAVDATA_HEADER  0x55667788
+
+
+typedef struct {
+  uint32_t    header;			/* NAVDATA_HEADER */
+  uint32_t    state;    /*!< Bit mask built from def_ardrone_state_mask_t */
+  uint32_t    sequence;         /*!< Sequence number, incremented for each sent packet */
+  uint32_t    vision_defined;
+  char options[];
+} __attribute__((packed)) NavHeader;
+
+
+#define ARDRONE_STATE_ELEMS(_) \
+_(DRONE_FLY_MASK, 0) /*!< FLY MASK : (0) ardrone is landed, (1) ardrone is flying */ \
+_(VIDEO_MASK, 1) /*!< VISION MASK : (0) vision disable, (1) vision enable */ \
+_(VISION_MASK, 2) /*!< VISION MASK : (0) vision disable, (1) vision enable */ \
+_(CONTROL_MASK, 3) /*!< CONTROL ALGO : (0) euler angles control, (1) angular speed control */ \
+_(ALTITUDE_MASK, 4) /*!< ALTITUDE CONTROL ALGO : (0) altitude control inactive (1) altitude control active */ \
+_(USER_FEEDBACK_START, 5) /*!< USER feedback : Start button state */ \
+_(COMMAND_MASK, 6) /*!< Control command ACK : (0) None, (1) one received */ \
+_(FW_FILE_MASK, 7) /* Firmware file is good (1) */ \
+_(FW_VER_MASK, 8) /* Firmware update is newer (1) */ \
+_(NAVDATA_DEMO_MASK, 10) /*!< Navdata demo : (0) All navdata, (1) only navdata demo */ \
+_(NAVDATA_BOOTSTRAP, 11) /*!< Navdata bootstrap : (0) options sent in all or demo mode, (1) no navdata options sent */ \
+_(MOTORS_MASK, 12) /*!< Motors status : (0) Ok, (1) Motors problem */ \
+_(COM_LOST_MASK, 13) /*!< Communication Lost : (1) com problem, (0) Com is ok */ \
+_(VBAT_LOW, 15) /*!< VBat low : (1) too low, (0) Ok */ \
+_(USER_EL, 16) /*!< User Emergency Landing : (1) User EL is ON, (0) User EL is OFF*/ \
+_(TIMER_ELAPSED, 17) /*!< Timer elapsed : (1) elapsed, (0) not elapsed */ \
+_(ANGLES_OUT_OF_RANGE, 19) /*!< Angles : (0) Ok, (1) out of range */ \
+_(ULTRASOUND_MASK, 21) /*!< Ultrasonic sensor : (0) Ok, (1) deaf */ \
+_(CUTOUT_MASK, 22) /*!< Cutout system detection : (0) Not detected, (1) detected */ \
+_(PIC_VERSION_MASK, 23) /*!< PIC Version number OK : (0) a bad version number, (1) version number is OK */ \
+_(ATCODEC_THREAD_ON, 24) /*!< ATCodec thread ON : (0) thread OFF (1) thread ON */ \
+_(NAVDATA_THREAD_ON, 25) /*!< Navdata thread ON : (0) thread OFF (1) thread ON */ \
+_(VIDEO_THREAD_ON, 26) /*!< Video thread ON : (0) thread OFF (1) thread ON */ \
+_(ACQ_THREAD_ON, 27) /*!< Acquisition thread ON : (0) thread OFF (1) thread ON */ \
+_(CTRL_WATCHDOG_MASK, 28) /*!< CTRL watchdog : (1) delay in control execution (> 5ms), (0) control is well scheduled */ \
+_(ADC_WATCHDOG_MASK, 29) /*!< ADC Watchdog : (1) delay in uart2 dsr (> 5ms), (0) uart2 is good */ \
+_(COM_WATCHDOG_MASK, 30) /*!< Communication Watchdog : (1) com problem, (0) Com is ok */ \
+_(EMERGENCY_MASK, 31) /*!< Emergency landing : (0) no emergency, (1) emergency */
+
+typedef enum {
+    #define MAKE_ENUM(name,bit) ND_##name = 1 << bit,
+    ARDRONE_STATE_ELEMS(MAKE_ENUM)
+    #undef MAKE_ENUM
+    NUM_BITS
+} StateMask;
+
+#define OPTION_TAGS(_) \
+_(DEMO,0) \
+_(TIME,1) \
+_(RAW_MEASURES,2) \
+_(PHYS_MEASURES,3) \
+_(GYROS_OFFSETS,4) \
+_(EULER_ANGLES,5) \
+_(REFERENCES,6) \
+_(TRIMS,7) \
+_(RC_REFERENCES,8) \
+_(PWM,9) \
+_(ALTITUDE,10) \
+_(VISION_RAW,11) \
+_(VISION_OF,12) \
+_(VISION,13) \
+_(VISION_PERF,14) \
+_(TRACKERS_SEND,15) \
+_(VISION_DETECT,16) \
+_(WATCHDOG,17) \
+_(ADC_DATA_FRAME,18) \
+_(VIDEO_STREAM,19) \
+_(GAMES,20) \
+_(PRESSURE_RAW,21) \
+_(MAGNETO,22) \
+_(WIND_SPEED,23) \
+_(KALMAN_PRESSURE,24) \
+_(HDVIDEO_STREAM,25) \
+_(WIFI,26) \
+_(ZIMMU_3000,27) \
+_(CKS,65535)
+
+typedef enum {
+#define MAKE_ENUM(name,num) OP_##name = num,
+    OPTION_TAGS(MAKE_ENUM)
+    NUM_OPTIONS
+} OptionTag;
+
+typedef float float9[9];
+typedef float float3[3];
+
+#define DEMO_STRUCT(_) \
+_(uint16_t,d,fly_state) \
+_(uint16_t,d,ctrl_state) \
+_(uint32_t,d,vbat_flying_percentage) \
+_(float,f,theta) \
+_(float,f,phi) \
+_(float,f,psi) \
+_(int32_t,d,altitude) \
+_(float,f,vx) \
+_(float,f,vy) \
+_(float,f,vz) \
+_(uint32_t,d,num_frames) \
+_(float9,p,detection_camera_rot) \
+_(float3,p,detection_camera_trans) \
+_(uint32_t,d,detection_tag_index) \
+_(uint32_t,d,detection_camera_type) \
+_(float9,p,drone_camera_rot) \
+_(float3,p,drone_camera_trans) \
+
+
+typedef struct {
+#define MAKESTRUCT(typ,fmt,name) typ name;
+
+DEMO_STRUCT(MAKESTRUCT)
+
+#undef MAKESTRUCT
+} DemoState;
+
+
+
+
 
 CFSocketRef atsocket;
 struct sockaddr_in ataddr;
@@ -44,6 +166,8 @@ float yaw = 0.0;
 float power = 0.0;
 int emergencyBit = 0;
 int flying = 0;
+uint32_t droneState;
+DemoState demoState;
 
 void gamepadAction(void* inContext, IOReturn inResult, 
                    void* inSender, IOHIDValueRef value) {
@@ -95,6 +219,20 @@ void gamepadAction(void* inContext, IOReturn inResult,
     
 }
 
+
+void PrintDemoDiff(DemoState * old, DemoState * cur) {
+    assert(sizeof(DemoState) + 4 == 148);
+    #define PRINT_DEMO(typ,fmt,field) if(cur->field != old->field && #fmt[0] != 'p') NSLog(@"%s = %" #fmt ";", #field, cur->field);
+    DEMO_STRUCT(PRINT_DEMO)
+    #undef PRINT_DEMO
+}
+void PrintStateDiff(lastState,curState) {
+    #define DO_PRINT(flag,bit) if( (curState & (1 << bit)) != (lastState & (1 << bit)))  NSLog(@"flag %s = %d", #flag, (curState & (1 << bit)) != 0);
+    ARDRONE_STATE_ELEMS(DO_PRINT)
+    #undef DO_PRINT
+}
+
+
 @implementation AppDelegate
 
 - (void)dealloc
@@ -108,42 +246,61 @@ int count = 0;
 int refflags = 0;
 int pcmdflags =  (1 << 1) | 1;
 
-- (void) sendToDrone: (id) what {
-    char buf[512];
-    
-    const char * fmt = "AT*REF=%d,%d\rAT*PCMD=%d,%d,%d,%d,%d,%d\r";
-    
-    refflags = (flying << 9) | (emergencyBit << 8);
-    sprintf(buf,fmt,count,refflags,count+1,pcmdflags,*(int*)&roll,*(int*)&pitch,*(int*)&power,*(int*)&yaw);
-    
-    NSData * data = [NSData dataWithBytes:buf length:strlen(buf)+1];
+
+int NewSeq() {
+    return ++count;
+}
+
+char sendbuf[512];
+int sendbuflen = 1; /* includes null*/
+int nsends = 0;
+void SendData(const char * fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    sendbuflen += vsprintf(sendbuf + sendbuflen - 1, fmt,ap);
+    va_end(ap);
+    nsends++;
+}
+
+DemoState oldDemoState;
+
+void FlushData() {
+    assert(sendbuflen == strlen(sendbuf) + 1);
+    NSData * data = [NSData dataWithBytes:sendbuf length:sendbuflen];
     NSData * destinationAddressData = [NSData dataWithBytes:&ataddr length:sizeof(ataddr)];
     
-    if(count/2 % 50 == 0) {//roughly every second, see what we are printing
-        NSLog(@"%s\n",buf);
-        #if 0
-        struct sockaddr_in myself;
-        initAddress(&myself, "127.0.0.1", 5554);
-        NSData * destinationAddressData2 = [NSData dataWithBytes:&myself length:sizeof(struct sockaddr_in)];
-        if(CFSocketSendData(navsocket,(CFDataRef)destinationAddressData2, (CFDataRef) data, 0))
-            NSLog(@"send error");
-        #endif
-    }
-    
-    
-    int result = CFSocketSendData(atsocket,(CFDataRef)destinationAddressData, (CFDataRef) data, 0);
-    count+=2;
-    if (result != 0) {
+    if(CFSocketSendData(atsocket,(CFDataRef)destinationAddressData, (CFDataRef) data, 0) ) {
         NSLog(@"BROKEN\n");
     }
+    if(count/2 % 50 == 0 || nsends > 1) {//roughly every second, see what we are printing
+        NSLog(@"%s\n",sendbuf);
+        PrintDemoDiff(&oldDemoState, &demoState);
+        memcpy(&oldDemoState,&demoState,sizeof(DemoState));
+    }
+    sendbuf[0] = '\0';
+    sendbuflen = 1;
+    nsends = 0;
+}
 
+
+
+/* run every 20ms to keep drone alive */
+- (void) updateState: (id) what {
+    const char * fmt = "AT*REF=%d,%d\rAT*PCMD=%d,%d,%d,%d,%d,%d\r";
+    refflags = (flying << 9) | (emergencyBit << 8);
+    int seq1 = NewSeq();
+    int seq2 = NewSeq();
+    SendData(fmt,seq1,refflags,seq2,pcmdflags,*(int*)&roll,*(int*)&pitch,*(int*)&power,*(int*)&yaw);
+    
+    FlushData();
+    
     const char * somedata = "1";
     NSData * data2 = [NSData dataWithBytes:somedata length:1];
     NSData * destinationAddressData2 = [NSData dataWithBytes:&navaddr length:sizeof(struct sockaddr_in)];
     if( 0 != CFSocketSendData(navsocket,(CFDataRef)destinationAddressData2, (CFDataRef) data2, 0)) {
         NSLog(@"Error sending data to nav port");
     }
-
+    
 }
 
 void initAddress(struct sockaddr_in * addr, const char * ip, int port) {
@@ -154,6 +311,14 @@ void initAddress(struct sockaddr_in * addr, const char * ip, int port) {
     inet_aton(ip,&addr->sin_addr);
 }
 
+typedef struct {
+    uint16_t tag;
+    uint16_t size;
+    char data[];
+} NavOption;
+
+
+
 void socketDataReceive(CFSocketRef s, CFSocketCallBackType type, CFDataRef address, const void *data, void *info) {
     char buf[512];
     int sock = CFSocketGetNative(s);
@@ -163,12 +328,63 @@ void socketDataReceive(CFSocketRef s, CFSocketCallBackType type, CFDataRef addre
     if (len == -1) {
         NSLog(@"recvfrom error");
     }
-    NSLog(@"SOCKET DATA: %d\n",(int)len);
-
+    //NSLog(@"DATA %d",len);
+    
+    
+    NavHeader * header = (NavHeader*) buf;
+    
+    if(header->header != NAVDATA_HEADER) {
+        NSLog(@"INCORRECT HEADER \n");
+        return;
+    }
+    
+    PrintStateDiff(droneState, header->state);
+    droneState = header->state;
+    
+    char * op = header->options;
+    while(op < buf + len) {
+        NavOption * navop = (NavOption*) op;
+        //NSLog(@"id = %d, size = %d\n",navop->tag,navop->size);
+        
+        #if 0
+        switch(navop->tag) {
+            #define GENSWITCH(name,id) case id: NSLog(@"Option: %s",#name); break;
+            OPTION_TAGS(GENSWITCH)
+            default: break;
+        }
+        #endif
+        switch(navop->tag) {
+            case OP_DEMO: {
+                DemoState * demo = (DemoState*) navop->data;
+                memcpy(&demoState,demo,sizeof(DemoState));
+            } break;
+            default:
+                break;
+        }
+        
+        
+        op += navop->size;
+    }
+    
+    
+    if(droneState & ND_NAVDATA_BOOTSTRAP) {
+        NSLog(@"BOOTSTRAP");
+        SendData("AT*CONFIG=%d,\"general:navdata_demo\",\"TRUE\"\r",NewSeq());
+    }
+#if 0
+    //this is in the spec but doesn't appear necessary...
+    if((droneState & ND_COMMAND_MASK) != 0 && len == 24) {
+        NSLog(@"ACKED");
+        SendData("AT*CTRL=0\r");
+    }
+#endif
+    
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    sendbuf[0] = '\0';
+    bzero(&demoState, sizeof(DemoState));
     // Insert code here to initialize your application
     //get a HID manager reference
     IOHIDManagerRef hidManager = IOHIDManagerCreate(kCFAllocatorDefault, 
@@ -244,7 +460,7 @@ void socketDataReceive(CFSocketRef s, CFSocketCallBackType type, CFDataRef addre
     
     [NSTimer scheduledTimerWithTimeInterval:0.02
                                  target:self
-                               selector:@selector(sendToDrone:)
+                               selector:@selector(updateState:)
                                userInfo:nil
                                 repeats:YES];
     
